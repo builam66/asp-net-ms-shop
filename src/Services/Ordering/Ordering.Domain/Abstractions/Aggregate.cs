94d9327b304a3129ee8de6ce2cdbb52faf363cab
Lam Bui Phuc <builam66@gmail.com>
2024-12-30 22:53:47 +0700

    ddd abstraction with entity, aggregate, domain event

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,23 @@
+﻿namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+    {
+        private readonly List<IDomainEvent> _domainEvents = [];
+
+        IReadOnlyList<IDomainEvent> IAggregate.DomainEvents => _domainEvents.AsReadOnly();
+
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        IDomainEvent[] IAggregate.ClearDomainEvents()
+        {
+            IDomainEvent[] dequeuedEvents = _domainEvents.ToArray();
+
+            _domainEvents.Clear();
+
+            return dequeuedEvents;
+        }
+    }
+}
