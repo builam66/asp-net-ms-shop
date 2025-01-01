e430d7cfb9ec97bca116b2195c0d732dbac504cc
Lam Bui Phuc <builam66@gmail.com>
2025-01-01 22:42:49 +0700

    implement strongly type for guid

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/StronglyType.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record OrderId { public Guid Value { get; } }
+
+    public record CustomerId { public Guid Value { get; } }
+
+    public record OrderItemId { public Guid Value { get; } }
+
+    public record ProductId { public Guid Value { get; } }
+}
