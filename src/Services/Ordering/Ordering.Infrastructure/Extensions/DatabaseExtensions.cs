90d83925fc2afc1e001e6f5ac0f56cbfa83af53f
Lam Bui Phuc <builam66@gmail.com>
2025-01-09 23:32:50 +0700

    auto migrate + seed data

---
+++ src/Services/Ordering/Ordering.Infrastructure/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure.Extensions
+{
+    public static class DatabaseExtensions
+    {
+        public static async Task InitializeDatabaseAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+            await SeedAsync(context);
+        }
+
+        private static async Task SeedAsync(ApplicationDbContext context)
+        {
+            await SeedCustomersAsync(context);
+            await SeedProductsAsync(context);
+            await SeedOrdersWithOrderItemsAsync(context);
+        }
+
+        private static async Task SeedCustomersAsync(ApplicationDbContext context)
+        {
+            if (!await context.Customers.AnyAsync())
+            {
+                await context.Customers.AddRangeAsync(InitialData.Customers);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        private static async Task SeedProductsAsync(ApplicationDbContext context)
+        {
+            if (!await context.Products.AnyAsync())
+            {
+                await context.Products.AddRangeAsync(InitialData.Products);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        private static async Task SeedOrdersWithOrderItemsAsync(ApplicationDbContext context)
+        {
+            if (!await context.Orders.AnyAsync())
+            {
+                await context.Orders.AddRangeAsync(InitialData.OrdersWithOrderItems);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Extensions/InitialData.cs
@@ -0,0 +1,56 @@
+﻿using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Extensions
+{
+    internal class InitialData
+    {
+        public static IEnumerable<Customer> Customers =>
+        [
+            Customer.Create(CustomerId.Of(new Guid("58c49479-ec65-4de2-86e7-033c546291aa")), "usera", "usera@gmail.com"),
+            Customer.Create(CustomerId.Of(new Guid("189dc8dc-990f-48e0-a37b-e6f2b60b9d7d")), "userb", "userb@gmail.com")
+        ];
+
+        public static IEnumerable<Product> Products =>
+        [
+            Product.Create(ProductId.Of(new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61")), "IPhone X", 500),
+            Product.Create(ProductId.Of(new Guid("c67d6323-e8b1-4bdf-9a75-b0d0d2e7e914")), "Samsung 10", 400),
+            Product.Create(ProductId.Of(new Guid("4f136e9f-ff8c-4c1f-9a33-d12f689bdab8")), "Huawei Plus", 650),
+            Product.Create(ProductId.Of(new Guid("6ec1297b-ec0a-4aa1-be25-6726e3b51a27")), "Xiaomi Mi", 450)
+        ];
+
+        public static IEnumerable<Order> OrdersWithOrderItems
+        {
+            get
+            {
+                var address1 = Address.Of("usera", "test", "usera@gmail.com", "Pham Van Dong", "Vietnam", "HCM", "12345");
+                var address2 = Address.Of("userb", "test", "userb@gmail.com", "QL13", "Vietnam", "HCM", "56789");
+
+                var payment1 = Payment.Of("usera", "0000000000000000", "01/30", "123", 1);
+                var payment2 = Payment.Of("userb", "1111111111111111", "01/30", "456", 2);
+
+                var order1 = Order.Create(
+                                OrderId.Of(Guid.NewGuid()),
+                                CustomerId.Of(new Guid("58c49479-ec65-4de2-86e7-033c546291aa")),
+                                orderName: "ORD_1",
+                                shippingAddress: address1,
+                                billingAddress: address1,
+                                payment1);
+                order1.Add(ProductId.Of(new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61")), 2, 500);
+                order1.Add(ProductId.Of(new Guid("c67d6323-e8b1-4bdf-9a75-b0d0d2e7e914")), 1, 400);
+
+                var order2 = Order.Create(
+                                OrderId.Of(Guid.NewGuid()),
+                                CustomerId.Of(new Guid("189dc8dc-990f-48e0-a37b-e6f2b60b9d7d")),
+                                orderName: "ORD_2",
+                                shippingAddress: address2,
+                                billingAddress: address2,
+                                payment2);
+                order2.Add(ProductId.Of(new Guid("4f136e9f-ff8c-4c1f-9a33-d12f689bdab8")), 1, 650);
+                order2.Add(ProductId.Of(new Guid("6ec1297b-ec0a-4aa1-be25-6726e3b51a27")), 2, 450);
+
+                return [order1, order2];
+            }
+        }
+    }
+}
