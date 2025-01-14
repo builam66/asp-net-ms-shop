2efbf2c9b79eac32bf33c6e51c3c1433c716a13f
Lam Bui Phuc <builam66@gmail.com>
2025-01-14 12:57:36 +0700

    some endpoints

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -4,11 +4,15 @@
     {
         public static IServiceCollection AddAPIServices(this IServiceCollection services)
         {
+            services.AddCarter();
+
             return services;
         }
 
         public static WebApplication UseAPIServices(this WebApplication app)
         {
+            app.MapCarter();
+
             return app;
         }
     }
---
+++ src/Services/Ordering/Ordering.API/Endpoints/CancelOrder.cs
@@ -0,0 +1,29 @@
+﻿using Ordering.Application.Order.Commands.CancelOrder;
+
+namespace Ordering.API.Endpoints
+{
+    //public record CancelOrderRequest(Guid Id);
+
+    public record CancelOrderResponse(bool IsSuccess);
+
+    public class CancelOrder : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/orders/{id:guid}/cancel", async (Guid Id, ISender sender) =>
+            {
+                var result = await sender.Send(new CancelOrderCommand(Id));
+
+                var response = result.Adapt<CancelOrderResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("CancelOrder")
+            .Produces<CancelOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Cancel Order")
+            .WithDescription("Cancel Order");
+        }
+    }
+}
